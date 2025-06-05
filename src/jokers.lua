4c060f341faa941d961f4ce631e539913b40c2d1
Erik Keller <erik@keller.nu>
2025-06-05 22:23:48 +0200

    Added mindoftony as mod author and added wild west

--- src/jokers.lua
+++ src/jokers.lua
@@ -1071,3 +1071,46 @@ SMODS.Joker {
         badges[#badges+1] = create_badge('by u/Thomassaurus', CREDIT_TEXT_BG_COLOR, CREDIT_TEXT_COLOR, CREDIT_TEXT_SIZE)
     end
 }
+
+-- Wild West
+SMODS.Joker {
+	key = 'wild_west',
+	blueprint_compat = true,
+	loc_txt = {
+		name = 'Wild West',
+		text = {
+			"{X:mult,C:white}X#1#{} Mult if played Hand",
+			"contains {C:attention}3 or more wild cards{}"
+		}
+	},
+
+	config = { extra = { Xmult = 3 } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult } }
+	end,
+
+	rarity = 2,
+	
+	atlas = 'reddit_jokers',
+	pos = { x = 5, y = 3 },
+
+	cost = 6,
+
+	calculate = function(self, card, context)
+        if context.joker_main then
+			local wild_cards = 0
+            for _, scored_card in ipairs(context.full_hand) do
+                if SMODS.has_enhancement(scored_card, 'm_wild') then wild_cards = wild_cards + 1 end
+            end
+			if wild_cards >= 3 then
+				return {
+					xmult = card.ability.extra.Xmult
+				}
+			end
+		end
+	end,
+
+    set_badges = function(self, card, badges)
+        badges[#badges+1] = create_badge('by u/GreedyHase', CREDIT_TEXT_BG_COLOR, CREDIT_TEXT_COLOR, CREDIT_TEXT_SIZE)
+    end
+}
