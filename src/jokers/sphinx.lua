cd6d62c26f75e026178719ae7dd618ae2db91263
Erik Keller <erik@keller.nu>
2025-06-11 18:02:02 +0200

    Add Sphinx of Black Quartz Joker
    - Added Sphinx of Black Quartz Joker
    - Updated Joker Textures

---
+++ src/jokers/sphinx.lua
@@ -0,0 +1,49 @@
+-- Sphinx of Black Quartz
+SMODS.Joker {
+	key = 'sphinx',
+	blueprint_compat = true,
+	loc_txt = {
+		name = 'Sphinx of Black Quartz',
+		text = {
+			"When {C:attention}Blind{} is selected,",
+            "create {C:attention}1{} {C:green}Uncommon{} {C:attention}Joker",
+            "{C:inactive}(Must have room)"
+		}
+	},
+
+	config = { extra = { } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = {  } }
+	end,
+
+	rarity = 2,
+	
+	atlas = 'reddit_jokers',
+	pos = { x = 2, y = 6 },
+
+	cost = 6,
+
+	calculate = function(self, card, context)
+        if context.setting_blind and #G.jokers.cards + G.GAME.joker_buffer < G.jokers.config.card_limit - 1 then
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    SMODS.add_card {
+                        set = 'Joker',
+                        rarity = 'Uncommon',
+                        key_append = 'reddit_sphinx' -- Optional, useful for manipulating the random seed and checking the source of the creation in `in_pool`.
+                    }
+                    G.GAME.joker_buffer = 0
+                    return true
+                end
+            }))
+            return {
+                message = localize('k_plus_joker'),
+                colour = G.C.BLUE,
+            }
+        end
+	end,
+
+    set_badges = function(self, card, badges)
+        badges[#badges+1] = credit_badge('Omicra98', false)
+	end
+}
