c68b4d1dba913b6fbcba70c0dfb6f5536ffd1914
Erik Keller <erik@keller.nu>
2025-06-11 17:53:59 +0200

    Added Crimson Dawn Joker
    - Added Crimson Dawn Joker
    - Updated Joker Textures
    - Updated Phoenix Joker Sprite Pos

---
+++ src/jokers/crimson_dawn.lua
@@ -0,0 +1,41 @@
+-- Crimson Dawn
+SMODS.Joker {
+	key = 'crimson_dawn',
+	blueprint_compat = true,
+	loc_txt = {
+		name = 'Crimson Dawn',
+		text = {
+			"Retrigger all played",
+            "cards in {C:attention}first",
+            "{C:attention}hand{} of round"
+		}
+	},
+
+	config = { extra = { active = false, repetitions = 1 } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = {  } }
+	end,
+
+	rarity = 2,
+	
+	atlas = 'reddit_jokers',
+	pos = { x = 0, y = 6 },
+
+	cost = 6,
+
+	calculate = function(self, card, context)
+        if context.setting_blind then
+            card.ability.extra.active = true
+
+        elseif context.repetition and context.cardarea == G.play and card.ability.extra.active then
+            card.ability.extra.active = false
+            return {
+                repetitions = card.ability.extra.repetitions
+            }
+        end
+	end,
+
+    set_badges = function(self, card, badges)
+        badges[#badges+1] = credit_badge('Omicra98', false)
+	end
+}
