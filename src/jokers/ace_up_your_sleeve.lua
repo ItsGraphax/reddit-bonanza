92ee6a6f53e6ce69cd9209af887be46d72d449b0
Erik Keller <erik@keller.nu>
2025-06-15 10:13:07 +0200

    v1.2.6 - Added Ace up your Sleeve Joker - Added Texture for Jokerdex Joker

---
+++ src/jokers/ace_up_your_sleeve.lua
@@ -0,0 +1,71 @@
+-- Ace up your Sleeve
+SMODS.Joker {
+    key = 'ace_up_your_sleeve',
+    blueprint_compat = true,
+    loc_txt = {
+        name = 'Ace up your Sleeve',
+        text = {
+            'When round begins,',
+            'add an {C:attention}Ace{} with',
+            'a random {C:attention} Edition{} and',
+            '{C:attention}Seal{} to your Hand'
+        }
+    },
+
+    config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = {  } }
+    end,
+
+    rarity = 3,
+
+    atlas = 'reddit_jokers',
+    pos = { x = 6, y = 4 },
+
+    cost = 8,
+
+    calculate = function(self, card, context)
+		local edition = poll_edition('reddit_ace_up_your_sleeve', nil, true, true)--, editions)
+        if context.first_hand_drawn then
+            local _card = SMODS.create_card {
+                set = "Base",
+                seal = SMODS.poll_seal({ guaranteed = true, type_key = 'reddit_ace_up_your_sleeve' }),
+                edition = edition,
+                area = G.discard
+            }
+
+            bases = {
+                G.P_CARDS["C_A"],
+                G.P_CARDS["S_A"],
+                G.P_CARDS["H_A"],
+                G.P_CARDS["D_A"]
+            }
+            index = math.floor(pseudorandom('reddit_ace_up_your_sleeve') * #bases) + 1
+            if index > #bases then
+                index = #bases
+            end
+            _card:set_base(bases[index], true)
+
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    G.hand:emplace(_card)
+                    _card:start_materialize()
+                    G.GAME.blind:debuff_card(_card)
+                    G.hand:sort()
+                    if context.blueprint_card then
+                        context.blueprint_card:juice_up()
+                    else
+                        card:juice_up()
+                    end
+                    return true
+                end
+            }))
+            SMODS.calculate_context({ playing_card_added = true, cards = { _card } })
+            return nil, true
+        end
+    end,
+
+    set_badges = function(self, card, badges)
+        badges[#badges+1] = credit_badge('TheUn-Nottened', false)
+    end
+}
