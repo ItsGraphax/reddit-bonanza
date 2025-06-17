0f47ad1f79704de1927e65972b427f7167afaf67
Eric Nwogbo <ericis2007@gmail.com>
2025-06-17 20:51:08 +0100

    Updated credit badges and joker textures for ring

--- src/jokers/phoenix.lua
+++ src/jokers/phoenix.lua
@@ -63,5 +63,6 @@ SMODS.Joker {
 	end,
     set_badges = function(self, card, badges)
         badges[#badges+1] = credit_badge('WarmTranslator6633', false)
+        badges[#badges+1] = create_badge('art by u/TSAMarioYTReddit', CREDIT_TEXT_BG_COLOR_ALT, CREDIT_TEXT_COLOR, CREDIT_TEXT_SIZE)
     end
 }
