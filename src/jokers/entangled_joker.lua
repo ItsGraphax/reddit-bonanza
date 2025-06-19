244afad97dc3c214579966ed06e65e839a0e3fbc
Eric Nwogbo <ericis2007@gmail.com>
2025-06-19 14:31:46 +0100

    Fixed entagnled joker
    - Fixed bug where copying the joker will crash the game
    - Fixed issue where only one entangled joker will load correctly if run is resumed
    - Updated display to give more information about the joker being copied (position and ability)
    - Updated the joker to be unable to copy incompatible jokers
    - Updated the joker to be unable to copy other entangled jokers

--- src/jokers/entangled_joker.lua
+++ src/jokers/entangled_joker.lua
@@ -1,6 +1,6 @@
 -- Entangled Joker
-local awaiting_load = false
-function get_joker_idx(joker)
+local awaiting_load = 0
+local get_joker_idx = function(joker)
     for idx, card in ipairs(G.jokers.cards) do
         if joker == card then
             return idx
@@ -9,14 +9,14 @@ function get_joker_idx(joker)
     return 0
 end
 
-local og_save_run = save_run
-function save_run()
+-- Syncs the indices of all entangled jokers
+local sync_ej = function ()
     -- load needs to be performed before we overwrite
-    if awaiting_load then
+    if awaiting_load > 0 then
         for ej, copied in pairs(G.GAME.r_entangled_indices) do
             G.jokers.cards[ej].ability.extra.other_joker = G.jokers.cards[copied]
+            awaiting_load = awaiting_load - 1
         end
-        awaiting_load = false
     end
     G.GAME.r_entangled_indices = {}
     for idx, card in ipairs(G.jokers.cards) do
@@ -24,9 +24,68 @@ function save_run()
             G.GAME.r_entangled_indices[idx] = get_joker_idx(card.ability.extra.other_joker)
         end
     end
+end
 
+local og_save_run = save_run
+function save_run()
+    sync_ej()
     og_save_run()
 end
+
+-- We need to override this as 
+-- copying entangled joker causes a stack overflow when copying the other jokers area
+local og_copy_card = copy_card
+function copy_card(other, new_card, card_scale, playing_card, strip_edition)
+    if other.config.center.key ~= 'j_reddit_entangled_joker' then 
+        return og_copy_card(other, new_card, card_scale, playing_card, strip_edition)
+    end
+    local new_card = new_card or Card(other.T.x, other.T.y, G.CARD_W*(card_scale or 1), G.CARD_H*(card_scale or 1), G.P_CARDS.empty, G.P_CENTERS.c_base, {playing_card = playing_card, bypass_back = G.GAME.selected_back.pos})
+    new_card:set_ability(other.config.center)
+    new_card.ability.type = other.ability.type
+    new_card:set_base(other.config.card)
+    for k, v in pairs(other.ability) do
+        if k == 'extra' then 
+            new_card.ability['extra'] = { other_joker = v.other_joker }
+        else
+            if type(v) == 'table' then 
+                new_card.ability[k] = copy_table(v)
+            else
+                new_card.ability[k] = v
+            end
+        end
+    end
+
+    if not strip_edition then 
+        new_card:set_edition(other.edition or {}, nil, true)
+        for k,v in pairs(other.edition or {}) do
+            if type(v) == 'table' then
+                new_card.edition[k] = copy_table(v)
+            else
+                new_card.edition[k] = v
+            end
+        end
+    end
+    check_for_unlock({type = 'have_edition'})
+    new_card:set_seal(other.seal, true)
+    if other.seal then
+        for k, v in pairs(other.ability.seal or {}) do
+            if type(v) == 'table' then
+                new_card.ability.seal[k] = copy_table(v)
+            else
+                new_card.ability.seal[k] = v
+            end
+        end
+    end
+    if other.params then
+        new_card.params = other.params
+        new_card.params.playing_card = playing_card
+    end
+    new_card.debuff = other.debuff
+    new_card.pinned = other.pinned
+    
+    return new_card
+end
+
 
 SMODS.Joker {
 	key = 'entangled_joker',
@@ -37,6 +96,7 @@ SMODS.Joker {
 			"Copies the ability",
             "of a random {C:attention}Joker{}, changes",
             "when blind is selected",
+            "{s:0.8}Cannot copy Entangled Joker{}",
             "{C:inactive}(Currently: #1#){}",
 		}
 	},
@@ -44,14 +104,14 @@ SMODS.Joker {
 	config = { extra = { other_joker = nil } },
     
     load = function (self, card, card_table, other_card)
-        awaiting_load = true
+        awaiting_load = awaiting_load + 1
         G.E_MANAGER:add_event(Event{
             func = function()
                 -- load has been taken care of
-                if awaiting_load == false then return true end
+                if awaiting_load <= 0 then return true end
                 local this_idx = get_joker_idx(card)
                 card.ability.extra.other_joker = G.jokers.cards[G.GAME.r_entangled_indices[this_idx]]
-                awaiting_load = false
+                awaiting_load = awaiting_load - 1
                 return true
             end
         })
@@ -89,11 +149,17 @@ SMODS.Joker {
             }
         } }
         if not card.ability.extra.other_joker then return { vars = { "None" } } end
+
+        info_queue[#info_queue+1] = copy_table(card.ability.extra.other_joker.config.center)
+        info_queue[#info_queue].config = card.ability.extra.other_joker.ability
+        
+        
 		return { vars = { localize{
             type = "name_text", 
             set = "Joker", 
             key = card.ability.extra.other_joker.config.center.key
-        }}, main_end = main_end }
+        } .. ' #' .. tostring(get_joker_idx(card.ability.extra.other_joker)) }, 
+        main_end = main_end }
 	end,
 
 	rarity = 3,
@@ -111,7 +177,10 @@ SMODS.Joker {
         if context.setting_blind and not context.blueprint then
             local valid_cards = {}
             for i = 1, #G.jokers.cards do
-                if G.jokers.cards[i] ~= card then
+                if 
+                    G.jokers.cards[i].config.center.key ~= card.config.center.key and
+                    G.jokers.cards[i].config.center.blueprint_compat
+                then
                     valid_cards[#valid_cards+1] = G.jokers.cards[i]
                 end
             end
