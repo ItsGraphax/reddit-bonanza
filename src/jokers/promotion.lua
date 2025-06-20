a11d69c904c920feca9f1fba673104df652be218
Erik Keller <erik[at]keller.nu>
2025-06-20 19:05:53 +0200

    v1.2.17

---
+++ src/jokers/promotion.lua
@@ -0,0 +1,37 @@
+-- Promotion
+SMODS.Joker {
+	key = 'promotion',
+	blueprint_compat = true,
+	loc_txt = {
+		name = 'Promotion',
+		text = {
+			"Increase Rank of",
+            "{C:attention}first{} played Card by {C:attention}1{}"
+		}
+	},
+
+	config = { extra = {  } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = {  } }
+	end,
+
+	rarity = 2,
+	
+	atlas = 'reddit_jokers',
+	pos = { x = 6, y = 6 },
+
+	cost = 6,
+
+	calculate = function(self, card, context)
+        if context.before and context.main_eval then
+            assert(SMODS.modify_rank(context.scoring_hand[1], 1))
+            return {
+                message = 'Promotion!'
+            }
+        end
+	end,
+
+    set_badges = function(self, card, badges)
+        badges[#badges+1] = credit_badge('USER', false)
+	end
+}
