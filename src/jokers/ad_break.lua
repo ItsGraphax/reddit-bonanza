16427a0bbdb0ef8c16ef985a5370bf85873100de
ItsGraphax <itsgraphax@proton.me>
2026-05-24 11:27:55 +0200

    Fix #23
    - Fixed ad break not giving end of round payout money

--- src/jokers/ad_break.lua
+++ src/jokers/ad_break.lua
@@ -9,10 +9,15 @@ SMODS.Joker {
 
     calculate = function(self, card, context)
         if context.after and not context.blueprint then
+            -- End of round (eor) calc
             SMODS.calculate_context({end_of_round = true, game_over = game_over })
+
+            -- Playing card eor
             for _,v in ipairs(SMODS.get_card_areas('playing_cards', 'end_of_round')) do
 			    SMODS.calculate_end_of_round_effects({ cardarea = v, end_of_round = true })
             end
+
+            -- Manual reset
             G.E_MANAGER:add_event(Event({
                 func = function ()
                     reset_idol_card()
@@ -27,6 +32,22 @@ SMODS.Joker {
                     return true
                 end 
             }))
+
+            -- Money gain
+            for _, card in ipairs(G.jokers.cards) do
+                if card.calculate_dollar_bonus and type(card.calculate_dollar_bonus) == 'function' then
+                    G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.3,
+                        blocking = true,
+                        func = function ()
+                            card:juice_up(0.3, 0.4)
+                            ease_dollars(card:calculate_dollar_bonus(card))
+                            return true
+                        end
+                    }))
+                end
+            end
 		end
 	end,
     set_badges = function(self, card, badges)
